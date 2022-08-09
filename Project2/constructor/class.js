38b9514e2d40f9d9de3a538d53b8b9c4a7549f58
77DDS77 <107913330+77DDS77@users.noreply.github.com>
2022-08-09 15:57:02 +0200

    merge login

---
+++ Project2/constructor/class.js
@@ -0,0 +1,19 @@
+//ricordarsi di usare id user e id shop per id su html 
+//ricordarsi di puntare agli accordion originali giusti
+//id e password da non mostrare su accordion
+//COMMENTARE
+
+
+//user creation class
+export class Utente{
+    constructor(id, username, firstName, lastName, gender, email, tel, password){
+
+    }
+}
+
+//shoop creation class
+export class Shop{
+    constructor(id, shopName, address, mail, tel, products){
+
+    }
+}
